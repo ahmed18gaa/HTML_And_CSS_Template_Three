c5f5871576fdd13da9f77433e787f4d629c642e3
ahmed18gaa <ahmed18ga@gmail.com>
2023-02-21 08:36:12 +0300

    Events Section JS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Skills Section
 let section = document.querySelector(".our-skills");
 let spans = document.querySelectorAll(".the-progress span");
 
@@ -8,3 +9,26 @@ window.onscroll = function () {
     });
   }
 };
+
+// Events Section
+let countDownDate = new Date("Dec 31, 2021 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  let dateNow = new Date().getTime();
+  let dateDiff = dateNow - countDownDate;
+
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) clearInterval(counter);
+}, 1000);
